76101e3423720f90f876ab5bf65652d5b10521f0
Darren Cole <cole.darren@gmail.com>
2018-09-08 19:33:34 -0400

    Assembled the layout components

--- layout_demo/lib/main.dart
+++ layout_demo/lib/main.dart
@@ -88,14 +88,6 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
@@ -106,10 +98,13 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
           children: [
             Image.asset(
               'images/lake.jpg',
+              width: 600.0,
               height: 240.0,
               fit: BoxFit.cover,
             ),
-            // ...
+            titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
