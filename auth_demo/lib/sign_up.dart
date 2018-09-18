55823fbff5a75321c838b4da9899c6b6a315444c
Darren Cole <cole.darren@gmail.com>
2018-09-18 10:40:28 -0400

    Removed an unused package import

--- auth_demo/lib/sign_up.dart
+++ auth_demo/lib/sign_up.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUp extends StatefulWidget {
