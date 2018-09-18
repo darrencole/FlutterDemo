cf7af6dbb7469f67fd8637fdf00849934401f9f0
Darren Cole <cole.darren@gmail.com>
2018-09-18 19:55:28 -0400

    Add forgot password form

--- auth_demo/lib/forgot_password.dart
+++ auth_demo/lib/forgot_password.dart
@@ -9,13 +9,64 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class ForgotPasswordState extends State<ForgotPassword> {
+  TextEditingController _emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    Widget _instructions = new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        new Container(
+          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 8.0),
+          child: new Text(
+            'Reset Password',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+        new Text(
+          '''Enter your email address then press "Submit".''',
+        ),
+      ],
+    );
+
+    Widget _signUpForm() {
+      return new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          new Container(
+            padding: const EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 15.0),
+            child: new TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                hintText: 'Email',
+              ),
+            ),
+          ),
+          new RaisedButton(
+            child: new Text('Submit'),
+            onPressed: () {
+              /*_handleCreateUserWithEmailAndPassword()
+              .then((FirebaseUser user) => _sendEmailVerification(user))
+              .catchError((e) => _handleExceptions(e));*/
+            },
+          ),
+        ],
+      );
+    }
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Auth Demo Forgot Password'),
       ),
-      body: new Text('Forgot Password'),
+      body: new Container(
+        padding: const EdgeInsets.fromLTRB(32.0, 4.0, 32.0, 32.0),
+        child: new ListView(
+          children: <Widget>[
+            _instructions,
+            _signUpForm(),
+          ],
+        ),
+      ),
     );
   }
 }
