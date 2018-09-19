da9e93db0cd14f562ecadd7ab5670b82d30ddbe7
Darren Cole <cole.darren@gmail.com>
2018-09-18 20:27:44 -0400

    Added verification message for password reset

--- auth_demo/lib/forgot_password.dart
+++ auth_demo/lib/forgot_password.dart
@@ -10,48 +10,88 @@ class ForgotPassword extends StatefulWidget {
 
 class ForgotPasswordState extends State<ForgotPassword> {
   TextEditingController _emailController = TextEditingController();
+  bool _showVerificationMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    //_errorMessage = '';
+    _showVerificationMessage = false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    Widget _instructions = new Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        new Container(
-          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 8.0),
-          child: new Text(
-            'Reset Password',
-            style: Theme.of(context).textTheme.title,
-          ),
-        ),
-        new Text(
-          '''Enter your email address then press "Submit".''',
-        ),
-      ],
+    Widget _subtitle = new Container(
+      padding: const EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 0.0),
+      child: new Text(
+        'Reset Password',
+        style: Theme.of(context).textTheme.title,
+      ),
     );
 
-    Widget _signUpForm() {
-      return new Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          new Container(
-            padding: const EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 15.0),
-            child: new TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                hintText: 'Email',
+    Widget _resetPasswordForm() {
+      if (_showVerificationMessage) {
+        return new Text('');
+      } else {
+        return new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new Container(
+              padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 0.0),
+              child: new Text(
+                '''Enter your email address then press "Submit".''',
               ),
             ),
-          ),
-          new RaisedButton(
-            child: new Text('Submit'),
-            onPressed: () {
-              /*_handleCreateUserWithEmailAndPassword()
+            new Container(
+              padding: const EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 15.0),
+              child: new TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+              ),
+            ),
+            new RaisedButton(
+              child: new Text('Submit'),
+              onPressed: () {
+                /*_handleCreateUserWithEmailAndPassword()
               .then((FirebaseUser user) => _sendEmailVerification(user))
               .catchError((e) => _handleExceptions(e));*/
-            },
-          ),
-        ],
-      );
+              },
+            ),
+          ],
+        );
+      }
+    }
+
+    Widget _verificationMessage() {
+      if (_showVerificationMessage) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new Container(
+              padding: const EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 15.0),
+              child: new Text(
+                '''An email was sent to your email address. Open the email and follow the instructions to reset your password.''',
+                style: new TextStyle(
+                  color: Colors.green,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                softWrap: true,
+              ),
+            ),
+            new RaisedButton(
+              child: new Text('Ok'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      } else {
+        return new Text('');
+      }
     }
 
     return new Scaffold(
@@ -62,8 +102,9 @@ class ForgotPasswordState extends State<ForgotPassword> {
         padding: const EdgeInsets.fromLTRB(32.0, 4.0, 32.0, 32.0),
         child: new ListView(
           children: <Widget>[
-            _instructions,
-            _signUpForm(),
+            _subtitle,
+            _resetPasswordForm(),
+            _verificationMessage(),
           ],
         ),
       ),
